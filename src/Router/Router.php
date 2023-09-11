b4e911bf39282b8f4e4586d5da70ebf53ef859c1
liutao <gg.cn.lt@gmail.com>
2023-09-11 18:15:29 +0800

    add getOrPost route method

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace MicroPHP\Framework\Router;
 
+use League\Route\RouteGroup;
+
 class Router extends \League\Route\Router
 {
+    public function getOrPost(string $path, $handler): RouteGroup
+    {
+        return $this->group('', function(RouteGroup $group) use ($path, $handler) {
+            $group->get($path, $handler);
+            $group->post($path, $handler);
+        });
+    }
 }
